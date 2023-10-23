a51d36eb0616a459fdd3397ee369f4902573f501
GangsterPairOfDice <daniel.lyakhovich@gmail.com>
2023-10-23 19:48:44 +0200

    Added preprocessing to emit code based on what operating system the user is on, since Windows automatically handles editing user input from terminal.

--- slic.c
+++ slic.c
@@ -1,11 +1,30 @@
 #include <stdio.h> // importing header which is a standard lib for common functions
 #include <stdlib.h> // gives us acess to the `free` function
 
-#include <editline/readline.h> // read input from some prompt, while allowing editing of the input
-//#include <editline/history.h> // lets us record history of inputs so we can retrieve with up and down arrow keys
+// if we are compiling on windows, compile these funcitons
+#ifdef _WIN32
+#include <string.h>
 
 // declare a buffer for user input of size 2048 characters
-static char input[2048]; // static controls visibility outside of file - cannot be used externally
+static char buffer[2048]; // static controls visibility outside of file - cannot be used externally
+
+// fake readline function
+char* readline(char* prompt) {
+  fputs(prompt, stdout); // outputs prompt
+  fgets(buffer, 2048, stdin); // gets input from user, and stores it in buffer
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy(strlen(cpy)-1) = '\0';
+  return cpy;
+}
+
+// fake add_history function
+void add_history(char* unused) {}
+
+// otherwise, include the editline headers
+#else
+#include <editline/readline.h> // read input from some prompt, while allowing editing of the input
+#endif
 
 int main(int argc, char** argv) {
 
