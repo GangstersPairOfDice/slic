3a5738abb89c383d3e15d95515d089cf6724cbac
GangsterPairOfDice <daniel.lyakhovich@gmail.com>
2023-10-29 16:10:11 +0100

    Updated to a more 'natural' grammar syntax (kek)

--- doge.c
+++ doge.c
@@ -10,36 +10,28 @@
 
 #include "mpc.h"
 
-// Build a parser 'Adjective' to recoqnize descriptions
-mpc_parser_t* Adjective = mpc_or(4,
-  mpc_sym("wow"),
-  mpc_sym("many"),
-  mpc_sym("so"),
-  mpc_sym("such")
-);
+mpc_parser_t* Adjective = mpc_new("adjective"); // Build a parser 'Adjective' to recoqnize descriptions
 
-// Build a parser 'Noun' to recognize things
-mpc_parser_t* Noun = mpc_or(5,
-  mpc_sym("lisp"),
-  mpc_sym("language"),
-  mpc_sym("c"),
-  mpc_sym("book"),
-  mpc_sym("build")
-);
+mpc_parser_t* Noun = mpc_new("noun"); // Build a parser 'Noun' to recognize things
 
-// Build a parser 'Phrase' to recognize things AND descriptions
-mpc_parser_t* Phrase = mpc_and(2,
-  mpcf_strfold,
-  Adjective,
-  Noun,
-  free
-);
+mpc_parser_t* Phrase = mpc_new("phrase"); // Build a parser 'Phrase' to recognize things AND descriptions
 
-// Build a parser 'Doge' to recognize zero or more Phrases
-mpc_parser_t* Doge = mpc_many(
-  mpcf_strfold,
-  Phrase
-);
+mpc_parser_t* Doge = mpc_new("doge"); // Build a parser 'Doge' to recognize zero or more Phrases
+
+mpca_lang(MPCA_LANG_DEFAULT,
+  "
+    adjective : \"wow\" | \"many\"            \
+              | \"so\" | \"such\";            \
+    noun      : \"lisp\" | \"language\"       \
+              | \"c\" | \"book\" | \"build\"; \
+    phrase    : <adjective> <noun>;           \
+    doge      : <phrase>*;                    \
+  ",
+  Adjective, Noun, Phrase, Doge);
+
+// Do some parsing here...
+
+mpc_cleanup(4, Adjective, Noun, Phrase, Doge);
 
 int main(int argc, char** argv) {
   return 0;
